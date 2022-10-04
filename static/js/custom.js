9d4f99b9a8038d5437bf01c91eae69a5723e25be
Daniel Easterman <daniel.easterman@gmail.com>
2022-10-04 17:52:47 +0100

    Smooth scroll down animation

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,17 +1,26 @@
-$(".reveal_all").click(function () {
-	const allPlayerFlex = $('.player_flex_wrapper');	
+$(".reveal_all").click(function (ev) {
+	const allPlayerFlex = $('.player_flex_wrapper');		
+	
+	if ( allPlayerFlex.is(":hidden") ) {
+		ev.preventDefault();
+		const hash = "#listings";
+		$("html, body").animate({scrollTop: $(hash).offset().top,},
+			1000, function () { window.location.hash = hash; });
+	}
 	
 	allPlayerFlex.slideToggle("fast", function() {
 		if ( allPlayerFlex.is(":visible") ) {
-			$('.reveal_all').html("Hide All Leaders &uarr;")
+			$('.reveal_all').html("Hide All Leaders &uarr;");
+			$('.reveal_specific').html("Hide Leaders &uarr;");
 		} else {
-			$('.reveal_all').html("Show All Leaders &darr;")
+			$('.reveal_all').html("Show All Leaders &darr;");
+			$('.reveal_specific').html("Show Leaders &uarr;");
+			
 		}
 	});
-
 });
 	
-$(".reveal_game_leaders").click(function () {
+$(".reveal_specific").click(function () {
 	const leaderButton = $(this);
 	const gameFlex = $(this).parents(".game_flex");
 	const playerFlex = $(gameFlex).next(".player_flex_wrapper");
@@ -23,5 +32,4 @@ $(".reveal_game_leaders").click(function () {
 			leaderButton.html("Show Leaders &darr;")
 		}
 	});
-
-});	
+});
