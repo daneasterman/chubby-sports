6243e5acb951c344875181cd69a64a16bb0f1076
Daniel Easterman <daniel.easterman@gmail.com>
2022-10-05 12:12:39 +0100

    More logic on show all / show specific

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,35 +1,32 @@
 $(".reveal_all").click(function (ev) {
-	const allPlayerFlex = $('.player_flex_wrapper');		
-	
-	if ( allPlayerFlex.is(":hidden") ) {
-		ev.preventDefault();
-		const hash = "#listings";
-		$("html, body").animate({scrollTop: $(hash).offset().top,},
-			1000, function () { window.location.hash = hash; });
-	}
-	
-	allPlayerFlex.slideToggle("fast", function() {
-		if ( allPlayerFlex.is(":visible") ) {
-			$('.reveal_all').html("Hide All Leaders &uarr;");
-			$('.reveal_specific').html("Hide Leaders &uarr;");
-		} else {
-			$('.reveal_all').html("Show All Leaders &darr;");
-			$('.reveal_specific').html("Show Leaders &uarr;");
-			
-		}
-	});
+	const allPlayerFlex = $('.player_flex_wrapper');	
+	 // If any player divs are hidden:
+	if ( allPlayerFlex.is(":hidden") ) {	
+			smoothScrollDown(ev)
+			allPlayerFlex.slideDown("fast", function() {});
+			$('.reveal_specific').html("Hide Leaders &uarr;");				
+		 } else {
+			smoothScrollDown(ev)
+		 }
 });
 	
 $(".reveal_specific").click(function () {
 	const leaderButton = $(this);
 	const gameFlex = $(this).parents(".game_flex");
-	const playerFlex = $(gameFlex).next(".player_flex_wrapper");
-	
-	playerFlex.slideToggle("fast", function() {
-		if ( playerFlex.is(":visible") ) {
-			leaderButton.html("Hide Leaders &uarr;")
-		} else {
-			leaderButton.html("Show Leaders &darr;")
-		}
-	});
+	const specificPlayerFlex = $(gameFlex).next(".player_flex_wrapper");
+
+	if ( specificPlayerFlex.is(":hidden") ) {
+		specificPlayerFlex.slideDown("fast", function() {});
+		leaderButton.html("Hide Leaders &uarr;")
+	} else {
+		specificPlayerFlex.slideUp("fast", function() {});
+		leaderButton.html("Show Leaders &darr;")		
+	}
 });
+
+function smoothScrollDown(ev) {
+	ev.preventDefault();
+	const hash = "#listings";
+	$("html, body").animate({scrollTop: $(hash).offset().top,},
+		1000, );
+}
