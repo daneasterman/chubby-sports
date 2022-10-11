823ee274a499200c8e0f8e727be8454a4a60a797
Daniel Easterman <daniel.easterman@gmail.com>
2022-10-11 12:31:09 +0100

    Add Heroku-specific config, Procfile and runtime files

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from pprint import pprint
 from flask import Flask, render_template
 from nfl_api.games import get_games
 from pprint import pprint
 
 app = Flask(__name__)
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def home():
--- nfl_api/games.py
+++ nfl_api/games.py
@@ -26,7 +26,7 @@ def get_games():
 			home_team = c["competitors"][0]
 			away_team = c["competitors"][1]
 			
-			raw_datestring = c["date"]		
+			raw_datestring = c["date"]
 			utc_obj = parser.parse(raw_datestring)			
 			usa_eastern_timezone = tz.gettz('America/New_York')
 			usa_eastern_datetime = utc_obj.astimezone(usa_eastern_timezone)
