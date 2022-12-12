a1d79ccc219c45f0fee0d00e9498cc419675eca8
Daniel Easterman <daniel.easterman@gmail.com>
2022-12-12 18:32:04 +0000

    Reorganise templates to make structure clearer

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pprint
 from datetime import datetime, timezone
 from flask import Flask, render_template
 from espn_api.nfl_games import get_nfl_games
@@ -7,7 +6,6 @@ from espn_api.nba_games import get_nba_games
 from espn_api.soccer_games import get_soccer_games
 from espn_api.custom_utils import get_est_datetime
 from flask_talisman import Talisman
-
 from pprint import pprint
 
 app = Flask(__name__)
@@ -17,24 +15,19 @@ app.config.from_object(env_config)
 @app.route("/")
 def nfl():
 	info = get_nfl_games()
-	return render_template("globals/games.html", info=info, title="NFL Leaders")
+	return render_template("nfl/base.html", info=info, title="NFL Leaders")
 
 @app.route("/nba")
 def nba():
 	info = get_nba_games()
-	return render_template("globals/games.html", info=info, title="NBA Leaders")
+	return render_template("nba/base.html", info=info, title="NBA Leaders")
 
 @app.route("/worldcup")
 def worldcup():
 	WORLDCUP_CODE = "fifa.world"
 	date_range = "20221213-20221216"	
-	games = get_soccer_games(WORLDCUP_CODE, date_range)
-	
-	return render_template(
-		"/soccer/worldcup/base.html", 
-		info=games,
-		title="World Cup Scorers")
-
+	info = get_soccer_games(WORLDCUP_CODE, date_range)	
+	return render_template("/soccer/worldcup/base.html", info=info, title="World Cup Scorers")
 
 if os.getenv("PROD_APP_SETTINGS"):
 	Talisman(app, content_security_policy=None)
