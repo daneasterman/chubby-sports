c40bad862cf168d165ceace90cf382dbff0b7c18
Daniel Easterman <daniel.easterman@gmail.com>
2023-02-19 18:07:45 +0000

    All Star game new data hotfix

--- espn_api/nba_games.py
+++ espn_api/nba_games.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 def get_nba_games():
 	today_custom_str, today_trunc_custom_str = get_est_datetime()
-	NBA_URL = f"{BASE_ESPN}/basketball/nba/scoreboard?dates={today_trunc_custom_str}"
+	NBA_URL = f"{BASE_ESPN}/basketball/nba/scoreboard?dates={today_trunc_custom_str}"	
 	nba_raw = requests.get(NBA_URL).json()
 	events = nba_raw['events']
 	
@@ -22,70 +22,77 @@ def get_nba_games():
 		competitions = e["competitions"]
 		for c in competitions:
 			home_team = c["competitors"][0]
-			away_team = c["competitors"][1]	
+			away_team = c["competitors"][1]
+			try: 
+				home_leaders = home_team.get("leaders")
+				away_leaders = away_team.get("leaders")
+				
+				home_points_leader = [h for h in home_leaders if h.get("name") == 'pointsPerGame']			
+				home_assists_leader = [h for h in home_leaders if h.get("name") == 'assistsPerGame']
+				home_rebounds_leader = [h for h in home_leaders if h.get("name") == 'reboundsPerGame']	
 
-			home_leaders = home_team["leaders"]
-			away_leaders = away_team["leaders"]
-			
-			home_points_leader = [h for h in home_leaders if h["name"] == 'pointsPerGame']			
-			home_assists_leader = [h for h in home_leaders if h["name"] == 'assistsPerGame']
-			home_rebounds_leader = [h for h in home_leaders if h["name"] == 'reboundsPerGame']	
-
-			away_points_leader = [a for a in away_leaders if a["name"] == 'pointsPerGame']			
-			away_assists_leader = [a for a in away_leaders if a["name"] == 'assistsPerGame']
-			away_rebounds_leader = [a for a in away_leaders if a["name"] == 'reboundsPerGame']
-			
+				away_points_leader = [a for a in away_leaders if a.get("name") == 'pointsPerGame']			
+				away_assists_leader = [a for a in away_leaders if a.get("name") == 'assistsPerGame']
+				away_rebounds_leader = [a for a in away_leaders if a.get("name") == 'reboundsPerGame']					
+			except:
+				pass
 			
 			uk_pretty_time, _, _ = get_pretty_custom(c["date"], LONDON)
-			est_pretty_time, _, _ = get_pretty_custom(c["date"], NEW_YORK)		
+			est_pretty_time, _, _ = get_pretty_custom(c["date"], NEW_YORK)
 
-			game = {
-					"home_team": {
-						"name": home_team["team"]["displayName"],
-						"score": home_team["score"],
-						"logo": home_team["team"]["logo"],
-						"record": home_team["records"][0]["summary"],						
-						"leaders": {
-							"points": {
-								"data": home_points_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(home_points_leader[0]["leaders"][0]["athlete"]["displayName"])
+			# a if condition else b
+			try:
+				game = {
+						"home_team": {
+							"name": home_team["team"]["displayName"],
+							"score": home_team["score"],
+							"logo": home_team["team"]["logo"],
+							"record": home_team.get("records")[0].get("summary"),
+							"leaders": {
+								"points": {
+									"data": home_points_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(home_points_leader[0]["leaders"][0]["athlete"]["displayName"])
+								},
+								"assists": {
+									"data": home_assists_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(home_assists_leader[0]["leaders"][0]["athlete"]["displayName"])
+								},
+								"rebounds": {
+									"data": home_rebounds_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(home_rebounds_leader[0]["leaders"][0]["athlete"]["displayName"])
+								}
 							},
-							"assists": {
-								"data": home_assists_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(home_assists_leader[0]["leaders"][0]["athlete"]["displayName"])
+						},					
+						"away_team": {
+							"name": away_team["team"]["displayName"],
+							"score": away_team["score"],
+							"logo":  away_team["team"]["logo"],
+							"record": away_team["records"][0]["summary"],
+							"leaders": {
+								"points": {
+									"data": away_points_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(away_points_leader[0]["leaders"][0]["athlete"]["displayName"])
+								},
+								"assists": {
+									"data": away_assists_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(away_assists_leader[0]["leaders"][0]["athlete"]["displayName"])
+								},
+								"rebounds": {
+									"data": away_rebounds_leader[0]["leaders"][0]["athlete"],
+									"wiki": make_wiki_link(away_rebounds_leader[0]["leaders"][0]["athlete"]["displayName"])
+								}
 							},
-							"rebounds": {
-								"data": home_rebounds_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(home_rebounds_leader[0]["leaders"][0]["athlete"]["displayName"])
-							}
-						},
-					},					
-					"away_team": {
-						"name": away_team["team"]["displayName"],
-						"score": away_team["score"],
-						"logo":  away_team["team"]["logo"],
-						"record": away_team["records"][0]["summary"],
-						"leaders": {
-							"points": {
-								"data": away_points_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(away_points_leader[0]["leaders"][0]["athlete"]["displayName"])
 							},
-							"assists": {
-								"data": away_assists_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(away_assists_leader[0]["leaders"][0]["athlete"]["displayName"])
-							},
-							"rebounds": {
-								"data": away_rebounds_leader[0]["leaders"][0]["athlete"],
-								"wiki": make_wiki_link(away_rebounds_leader[0]["leaders"][0]["athlete"]["displayName"])
-							}
-						},
-						},
-					"uk_time": uk_pretty_time,
-					"est_time": est_pretty_time,					
-					"stadium": c["venue"]["fullName"],
-					"city": c["venue"]["address"]["city"]
-			}
-			nba_clean['games'].append(game)
+						"uk_time": uk_pretty_time,
+						"est_time": est_pretty_time,					
+						"stadium": c["venue"]["fullName"],
+						"city": c["venue"]["address"]["city"]
+					}
+				nba_clean['games'].append(game)
+			except:
+				pass
+				
+			
 
 	# with open('test_v1.json', 'w') as outfile:
 	# 	json.dump(nba_clean, outfile, indent=2)
